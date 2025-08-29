65899117c927624df883a2e9d815091c9d5d4886
raihan-k-rasily <raihankrasily@gmail.com>
2025-08-29 10:27:05 +0530

    rBuilderServer

---
+++ index.js
@@ -0,0 +1,20 @@
+// import json server
+const JSONServer = require('json-server')
+// create server for running json file
+const rBuilderServer = JSONServer.create()
+// import 'db.json'
+const router = JSONServer.router('db.json')
+// create a middleware
+const middleware = JSONServer.defaults()
+// define port to run the app
+const PORT = 3000 || process.env.PORT
+// use middleware json server
+rBuilderServer.use(middleware)
+// define routes for client request
+rBuilderServer.use(router) 
+
+// run Server for client request 
+rBuilderServer.listen(PORT,()=>{
+    console.log(`rBuilderServer started at port: ${PORT} and waiting for client request !!!`);
+    
+})
